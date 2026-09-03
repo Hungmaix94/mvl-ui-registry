6e87ed522bc92b3559d5aa075093fbec7364b9e1
Hungmaix94 <phamhung.bk94@gmail.com>
2026-09-03 11:17:38 +0700

    fix: add tailwindcss vite plugin to storybook config

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,3 +1,4 @@
+import tailwindcss from "@tailwindcss/vite";
 import type { StorybookConfig } from '@storybook/react-vite';
 import { mergeConfig } from 'vite';
 import tsconfigPaths from 'vite-tsconfig-paths';
@@ -16,7 +17,7 @@ const config: StorybookConfig = {
   "framework": "@storybook/react-vite",
   async viteFinal(config) {
     return mergeConfig(config, {
-      plugins: [tsconfigPaths()],
+      plugins: [tailwindcss(), tsconfigPaths()],
     });
   },
 };
